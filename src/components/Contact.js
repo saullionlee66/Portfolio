68e8a64fc4e658b2736686969ed9024063d9ff36
Saul <saullionlee@hotmail.com>
2021-08-03 21:23:04 -0400

    font color adjusted

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { Text, VStack, Heading, Box, Divider } from "@chakra-ui/react";
+import { Text, VStack, Heading, Box, useColorMode } from "@chakra-ui/react";
 
 function Contact() {
+	const { colorMode } = useColorMode();
 	return (
 		<VStack
+			color={colorMode === "light" ? "#5121A0" : "black"}
 			p={10}
 			w='100%'
 			h='84vh'
 			bgImage='https://lh3.googleusercontent.com/lMWP55MGrRZFKZkLHXD108rKWMoPjKsQTqBZoapYLXtlWFXxEW-JwDEXryFp9w2b-_i6L1fPpM56kp65iQmkiAZt3kWoClFCdEFqh9JBxpr8aTO1yIs4GnyF7L2FfC7x6IDML7bpq5Q=w2400'
-			bgPosition='center'>
+			bgPosition='center'
+			bgRepeat='no-repeat'>
 			<Box
 				fontFamily='Cambria'
 				p={6}
 				borderRadius='lg'
-				mt={4}
+				mt='200'
 				textAlign='center'
 				w='100%'
 				maxW={{ xm: "90vw", sm: "80vw", lg: "50vw", xl: "40vw" }}>
@@ -29,26 +32,16 @@ function Contact() {
 					fontWeight='bold'>
 					Address: 220 Newton Dr.{" "}
 				</Text>
-				<Text
-					color='primary.100'
-					fontSize={{ sm: "lg", lg: "xl", xl: "2xl" }}
-					fontWeight='bold'>
+				<Text fontSize={{ sm: "lg", lg: "xl", xl: "2xl" }} fontWeight='bold'>
 					Toronto Ontario, M2M 2P3
 				</Text>
 
-				<Text
-					color='primary.100'
-					fontSize={{ sm: "lg", lg: "xl", xl: "2xl" }}
-					fontWeight='bold'>
+				<Text fontSize={{ sm: "lg", lg: "xl", xl: "2xl" }} fontWeight='bold'>
 					Mobile: 514-660-7110
 				</Text>
-				<Text
-					color='primary.100'
-					fontSize={{ sm: "lg", lg: "xl", xl: "2xl" }}
-					fontWeight='bold'>
+				<Text fontSize={{ sm: "lg", lg: "xl", xl: "2xl" }} fontWeight='bold'>
 					Email: saullionlee@gmail.com
 				</Text>
-				<Divider borderColor='primary.200' border='2px' />
 				<Heading
 					fontSize={{ sm: "3xl", lg: "2xl", xl: "6xl" }}
 					fontWeight='extrabold'>
